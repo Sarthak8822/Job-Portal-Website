9931a2697570bb6daa3c03e03d0a845ead8badbe
Sarthak <sarthakpravin08@gmail.com>
2023-08-30 23:57:00 +0530

    demo staging

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,11 @@ export default function ProfilePage() {
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Logout
+            </button>
         </div>
     )
 }
